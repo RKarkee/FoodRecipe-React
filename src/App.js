702993dabd9283b2659234abf8abfb1103918976
RKarkee <rajankarkee01@gmail.com>
2022-02-27 17:42:33 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import RecipeTile from './RecipeTile';
 function App() {
   const [query, setquery]=useState("");
   const [recipes, setrecipes]=useState([])
+  const [healthLabels, sethealthLabels]= useState("vegan")
+
   const YOUR_APP_ID = process.env.REACT_APP_ID
   const YOUR_APP_KEY= process.env.REACT_APP_KEY
 
   var url = `https://api.edamam.com/search?q=${query}&
-app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=21&health=${healthLabels}`;
   
   async function getRecipes(){
     var result = await Axios.get(url); 
@@ -34,10 +36,19 @@ app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&healt
         value={query} onChange={(e)=> setquery(e.target.value)}
         />
         <input className='app__submit' type="submit" value="Search" />
+        <select className='app__healthLabels'>
+          <option onClick={()=> sethealthLabels("vegan")}>Vegan</option>
+          <option onClick={()=> sethealthLabels("vegetarian")}>Vegetarian</option>
+          <option onClick={()=> sethealthLabels("paleo")}>Paleo</option>
+          <option onClick={()=> sethealthLabels("gluten-free")}>Gluten-Free</option>
+          <option onClick={()=> sethealthLabels("wheat-free")}>Wheat-Free</option>
+          <option onClick={()=> sethealthLabels("egg-free")}>Egg-Free</option>
+          <option onClick={()=> sethealthLabels("low-sugar")}>Low-Sugar</option>
+        </select>
       </form>
-      <div>
+      <div className='app__recipes'>
       {recipes.map((recipe) =>{
-        return <RecipeTile recipe={recipe} />;
+        return <RecipeTile key={recipe.totalWeight} recipe={recipe} />;
       })}
       </div>
     </div>
